a07a8c035ca26873bd19ffc847f43e1e9039e217
hajirazin <hajirazin@hotmail.com>
2014-12-09 14:39:24 +0530

    Created first copy

---
+++ EnumGenerator/Directions.cs
@@ -0,0 +1,15 @@
+﻿
+namespace T4Practice
+{
+public enum DirectionEnum
+{
+	North                                              = 1, 
+	East                                               = 2, 
+	West                                               = 3, 
+	South                                              = 4, 
+	NorthEast                                          = 5, 
+	NorthWest                                          = 6, 
+	SouthEast                                          = 7, 
+	SouthWest                                          = 8, 
+}
+}
